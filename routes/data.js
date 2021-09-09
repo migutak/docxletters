26c54f0e34e074f6a7d3dc3705231717667e7ba8
Kevin Miguta <migutak@gmail.com>
2021-09-09 11:05:31 +0300

    routes

--- routes/data.js
+++ routes/data.js
@@ -1,6 +1,6 @@
 module.exports = {
     // filePath        : process.env.FILEPATH || '/app/nfsmount/demandletters/', // "d:\\demands\\",
-    filePath        : process.env.FILEPATH || "d:\\demands\\",
+    filePath        : process.env.FILEPATH || "C:\\Users\\pap\\Documents\\demands",
     imagePath       : process.env.IMAGEPATH || '/app/docxletters/routes/', // 'd:\\angularprojects\\docxletters\\routes\\',
     SENDEMAILURL     : process.env.SENDEMAILURL || 'http://172.16.204.72:8005/ipfcancellation/email'
 };
