13b845711f8807d5bd9acfb613d262f6bb9a0d4d
Kevin Miguta Abongo <kevin.abongo@royalcyber.com>
2022-05-19 15:10:59 +0300

    routes

--- routes/data.js
+++ routes/data.js
@@ -1,6 +1,6 @@
 module.exports = {
     // filePath        : process.env.FILEPATH || '/app/nfsmount/demandletters/', // "d:\\demands\\",
-    filePath        : process.env.FILEPATH || "C:/Users/allan/Videos/generatedletters/",
+    filePath        : process.env.FILEPATH || "C:/demands/",
     imagePath       : process.env.IMAGEPATH || '/app/docxletters/routes/', // 'd:\\angularprojects\\docxletters\\routes\\',
     SENDEMAILURL     : process.env.SENDEMAILURL || 'http://172.16.204.72:8005/ipfcancellation/email',
     smtpport: process.env.SMTPPORT || 587,  // 465 FOR SECURE OR PROD
